bd0ccd253c03c0b356c12488d2e034e4b88aee24
Ayush Tripathi <ayushtripathi2822@gmail.com>
2026-04-25 09:01:22 +0530

    Update TypingEffect.tsx

--- frontend/src/components/ui/TypingEffect.tsx
+++ frontend/src/components/ui/TypingEffect.tsx
@@ -1,1 +1,31 @@
+"use client";
 
+import { useEffect, useState } from "react";
+
+type Props = {
+  text: string;
+  speed?: number;
+};
+
+export default function TypingEffect({ text, speed = 15 }: Props) {
+  const [displayed, setDisplayed] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+
+    setDisplayed("");
+
+    const interval = setInterval(() => {
+      setDisplayed((prev) => prev + text.charAt(i));
+      i++;
+
+      if (i >= text.length) {
+        clearInterval(interval);
+      }
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [text, speed]);
+
+  return <pre className="whitespace-pre-wrap">{displayed}</pre>;
+}
